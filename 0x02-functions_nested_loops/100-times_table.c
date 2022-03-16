4461163a0a8808fe9cd59c1b23898c8c8d09b426
YonathanDawit <99809306+YonathanDawit@users.noreply.github.com>
2022-03-16 07:34:22 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,49 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - program that prints all possible different combinations of two digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Return: 0
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
+void print_times_table(int n)
+{
+	int x = 0, y, z;
 
-int main(void)
-{
-int n1 = 48;
-int n2;
-int com = 44;
-while (n1 <= 57)
-{
-n2 = n1 + 1;
-while (n2 <= 57)
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 56 || n2 != 57)
-{
-putchar(com);
-putchar(32);
-}
-n2 += 1;
-}
-n1 += 1;
-}
-putchar('\n');
-return (0);
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
 }
