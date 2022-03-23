13c348f900915f4d4f25804c8ef52b9038601415
YonathanDawit <99809306+YonathanDawit@users.noreply.github.com>
2022-03-23 17:05:13 +0300

    More

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- *  * _strncpy - Copy a string starting from index 0 of `dest`.
- *   * @dest: string
- *    * @src: string
- *     * @n: number of chars to copy over
- *      * Return: `dest` edited string
- *       */
+ * _strncpy - Copy a string starting from index 0 of `dest`.
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy over
+ * Return: `dest` edited string
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-                int i;
+	int i;
 
-                        for (i = 0; i < n && src[i] != '\0'; i++)
-                                                dest[i] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-                                for (; n > i; i++)
-                                                        dest[i] = '\0';
+	for (; n > i; i++)
+		dest[i] = '\0';
 
-                                        return (dest);
+	return (dest);
+}
