f20680536286cf1a73df7faa8428e71e83848fd4
YonathanDawit <99809306+YonathanDawit@users.noreply.github.com>
2022-04-14 23:56:32 +0300

    vf

--- 0x10. C - Variadic functions/variadic_functions.h
+++ 0x10. C - Variadic functions/variadic_functions.h
@@ -1,6 +1,3 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -9,10 +6,3 @@ void _printchar(va_list list);
 void _printstr(va_list list);
 void _printfloat(va_list list);
 void _printint(va_list list);
-
-typedef struct checker
-{
-	char *type;
-	void (*f)();
-} checker;
-#endif
